88ca6f9f31a1fea100349f7e7a95a27acb9532f7
Viktoriia <pecourses>
2022-05-14 17:33:16 +0300

    Refactor store

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './index.css';
-import Setup from './boot/setup';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-ReactDOM.render(<Setup />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
