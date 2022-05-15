c1be7ea95b53d135cd913e47dc8d31ac75cc91b7
Viktoriia <pecourses>
2022-05-15 17:56:40 +0300

    Add prices list

---
+++ client/src/pages/PracingPage/PricesList/PricesList.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import prices from './prices.json';
+import styles from './PricesList.module.sass';
+
+function PricesList () {
+  const mapPrices = (p, i) => (
+    <li key={i} className={styles.priceItem}>
+      <div style={{ border: `5px solid ${p.color}` }}>
+        <h3 style={{ color: `${p.color}` }}>{p.type}</h3>
+        <p>{p.describeType}</p>
+        <p style={{ color: `${p.color}` }}>{p.price}</p>
+      </div>
+
+      <ul>
+        {p.profit.map((pr, i) => (
+          <li key={i}>{pr.body}</li>
+        ))}
+      </ul>
+      <Link to='#' style={{ backgroundColor: `${p.color}` }}>
+        <i className='fa fa-check'></i> Start
+      </Link>
+    </li>
+  );
+
+  return <ul className={styles.pricesList}>{prices.map(mapPrices)}</ul>;
+}
+
+export default PricesList;
--- client/src/pages/PracingPage/PricingPage.js
+++ client/src/pages/PracingPage/PricingPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import PricesList from './PricesList/PricesList';
+import styles from './PricingPage.module.sass';
 
 function PricingPage () {
   return (
     <div>
       <Header />
-      PricingPage
+      <div className={styles.pageContainer}>
+        <PricesList />
+      </div>
+
       <Footer />
     </div>
   );
